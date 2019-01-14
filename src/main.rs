12d5ba26ce345515eb2c07faf1632e42ea248a12
Núria Balaguer <nyxscream@gmail.com>
2019-01-14 17:23:18 +0100

    clean the code and are one more endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
 extern crate actix_web;
 use actix_web::{server, App, HttpRequest};
 
-// struct AppState {
-//     counter: Cell<usize>,
-// }
-
-// fn index(_req: &HttpRequest) -> &'static str {
-//     // for _num in 1..10000000 {
-//     //     println!("Hello world");
-//     // }
-
-//     "Hello world"
-// }
-
-// fn index(req: &HttpRequest<AppState>) -> String {
-//     let count = req.state().counter.get() + 1; // <- get count
-//     req.state().counter.set(count); // <- store new count in state
-
-//     format!("Request number: {}", count) // <- response with count
-// }
+fn index(_req: &HttpRequest) -> &'static str {
+    "I'm working fine!!"
+}
 
 fn version(_req: &HttpRequest) -> &'static str {
     const VERSION: &'static str = env!("CARGO_PKG_VERSION");
@@ -28,25 +13,15 @@ fn version(_req: &HttpRequest) -> &'static str {
 fn main() {
 
     server::new(|| {
-        App::new()
-            .prefix("/version")
-            .resource("/latest", |r| r.f(version))
+        vec![
+            App::new()
+                .prefix("/version")
+                .resource("/latest", |r| r.f(version)),
+            App::new()
+                .resource("/", |r| r.f(index))
+        ]
     })
     .bind("127.0.0.1:8080")
     .unwrap()
     .run();
-
-    // server::new(|| {
-    //     App::with_state(AppState { counter: Cell::new(0) })
-    //         .resource("/", |r| r.method(http::Method::GET).f(index))
-    // })
-    // .bind("127.0.0.1:8080")
-    // .unwrap()
-    // .run()
 }
-
-// fn main() {
-//     for _num in 1..10000000 {
-//         println!("Hello world");
-//     }
-// }
